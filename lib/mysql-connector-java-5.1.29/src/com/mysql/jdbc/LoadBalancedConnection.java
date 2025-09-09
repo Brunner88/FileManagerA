1d611cd5a831dddaa409008cfdade6c80665703f
Alessandro Sbringhetti <alessandro.sbringhetti1991@gmail.com>
2025-09-09 22:51:03 +0200

    Aggiunta db e propperties per servizio

---
+++ lib/mysql-connector-java-5.1.29/src/com/mysql/jdbc/LoadBalancedConnection.java
@@ -0,0 +1,9 @@
+import java.sql.SQLException;
+
+public interface LoadBalancedConnection extends MySQLConnection {
+	
+	public boolean addHost(String host) throws SQLException;
+	public void removeHost(String host) throws SQLException;
+	public void removeHostWhenNotInUse(String host)	throws SQLException;
+
+}
--- src/utility/Utils.java
+++ src/utility/Utils.java
@@ -18,6 +18,28 @@ public class Utils {
         try {
             fis = new FileInputStream("config/generalConfig.properties");
         } catch (FileNotFoundException e) {
+            System.out.println("File non trovato");
+            System.exit(1);
+        }
+        Properties props = new Properties();
+        props.load(fis);
+
+        return props;
+    }
+
+    /**
+     * legge le configurazioni specifiche di un servizio
+     *
+     * @param service servizio da
+     * @return Properties contenente le proprietà caricate
+     * @throws IOException
+     */
+    public static Properties readProperties(String service) throws IOException {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("config/" + service + "_config.properties");
+        } catch (FileNotFoundException e) {
+            System.out.println("File non trovato");
             System.exit(1);
         }
         Properties props = new Properties();
